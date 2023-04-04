808737b038feae7a595340b5d606baa74cfc2900
Tobias Carlsson <janerlingtobias@gmail.com>
2023-04-04 22:58:37 +0200

    Jobbade mer på js-filen

---
+++ L1/js/script.js
@@ -0,0 +1,7 @@
+function init() { // deklarerar en funktion med namnet init, koden som initieras följer inom klammerparenteserna.
+
+}
+// ger t.ex. variabler initiala värden 
+
+
+init(); // När skriptfilen bearbetas och kommer hit körs initfunktionen
