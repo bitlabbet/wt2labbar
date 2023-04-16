4b7e22354b23dec8237456493ef6fb6782a1a2aa
Tobias Carlsson <janerlingtobias@gmail.com>
2023-04-16 18:47:42 +0200

    //JObbat med lab

--- L1/js/script.js
+++ L1/js/script.js
@@ -1,7 +1,33 @@
+var inp1Elem, inp2Elem; //variablerna deklareras här
+var resElem;
+
 function init() { // deklarerar en funktion med namnet init, koden som initieras följer inom klammerparenteserna.
-
+    inp1Elem = document.getElementById("input1"); //idattribut (input)
+    inp2Elem = document.getElementById("input2"); //idattribut (input) stavade fel till id:t här först och fick märkliga fel, men löste det genom att titta i konsollen.
+    resElem = document.getElementById("result"); //idattribut (div där resultaten ska visas)
+    document.getElementById("runBtn").onclick = areaCalculations; //idattribut (button)
+    
 }
-// ger t.ex. variabler initiala värden 
+window.onload = init; // När skriptfilen bearbetas och kommer hit körs initfunktionen
 
+function areaCalculations() {
+    let length; //deklarera variabel för längd
+    let width; //deklarerar variabel för bredd
+    let area; //deklarerar variabelk för area (yta i kvadratmeter)
 
-init(); // När skriptfilen bearbetas och kommer hit körs initfunktionen
+    length = Number(inp1Elem.value); //Läser in i textfältet för längd, konverteras till nummer
+    width = Number(inp2Elem.value); //Läser in i textfältet för bredd, konverteras till nummer
+    //Area för rektangel
+    area = length * width;
+    resElem.innerHTML = `<p>Rektangelns area är ${area} m<sup>2</sup></p>`; //VSC föreslog att jag skulle skriva strängen så här, tycker nästan att det blir tydligare så
+    //resELem-variabeln får varje gång ett nytt strängvärde som skrivs i div:en med id:t result
+    //Area för ellips
+    area = 3.14 * length * width / 4;
+    resElem.innerHTML += `<p>Ellipsens area är ${area} m<sup>2</sup></p>`; //fick först inte area-raden att visas, förstår varför jag behövde använda operatorn += för att första raden helt enkelt inte skulle skrivas över
+    //Arean av rektangel då längden ökas med 5 meter
+    area = (length + 5) * width; //variabeln length adderas med 5
+    resElem.innerHTML += "<p>Då längden ökas med 5m blir rektangelns area " + area + " m<sup>2</sup></p>";
+    //Egen kod för att beräkna area då längden ökas med 50 procent och bredden ökas med 3 meter
+    area = (length * 1.5) * (width + 3); // längeden * 1.5 för ökning med 50%
+    resElem.innerHTML += "<p>Då längden ökas med 50% och bredden med 3 meter blir rektangelns area " + area + " m<sup>2</sup></p>";
+}
